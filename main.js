14113799d1169d2055069b918b35d64c887fdc1d
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-07 15:30:58 +0200

    add reference

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log(jQuery); //test
+//REFERENCE
+var open = $('.header-right .fa-bars');
+var menu = $('.hamburger-menu');
+var close = $('.hamburger-menu .fa-times');
+
+console.log(open); //test
+console.log(menu); //test
+console.log(close); //test
