70e9c66f6fddbcb9ca5ba59d3b7d630aaa0f9514
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-07 15:33:37 +0200

    add show-hide menu

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ var open = $('.header-right .fa-bars');
 var menu = $('.hamburger-menu');
 var close = $('.hamburger-menu .fa-times');
 
-console.log(open); //test
-console.log(menu); //test
-console.log(close); //test
+//Show-Hide Menu
+open.click(function() {
+    menu.show();
+});
+close.click(function() {
+    menu.hide();
+});
